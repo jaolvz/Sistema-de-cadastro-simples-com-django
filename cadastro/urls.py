de62dcdc70190aec6caef0bbcdc72a2da0c9de8e
João <86298585+jaolvz@users.noreply.github.com>
2024-12-15 02:40:47 -0300

    inicio de cadastro

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('cadatro/cadastrar_usuario', views.cadastrar, name='cadastrar_usuario')
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Usuario 
 
 def home(request):
     return render(request, 'index.html')
+
+def cadastro(request):
+    return render(request,'cadastro.html')
+
+def cadastrar (request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif  request.method =='POST':
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        usuario = Usuario (usuario=usuario, email=email, senha=senha)
+        usuario.save()
+        return render(request,'index.html')
