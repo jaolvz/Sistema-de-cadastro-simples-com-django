0a9594f468c08d41159f7bc6e840efa4376a978a
João <86298585+jaolvz@users.noreply.github.com>
2024-12-18 05:13:24 -0300

    Implementação de pagina de perfl e posts

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ app_name ='home'
 urlpatterns = [
     path('', views.pag_inicial, name='pag_inicial'),
     path('perfil/', views.perfil,name='perfil'),
-    path('upload_post',views.upload_post, name='upload_post')
+    path('upload_post',views.upload_post, name='upload_post'),
     path('logout/', views.logout_sessao, name='logout')
 
 ]
